02e9226ac8f594e2e55daeb8487fe266a693ad0c
khayan <khayanmalantrucco@gmail.com>
2020-06-04 18:49:26 -0300

    State first steps

--- project-nlw-ecoleta/web/src/App.tsx
+++ project-nlw-ecoleta/web/src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+  
   return (
     <div>
-      <Header title="Ecoleta"/>
+      <Header title={`Ecoleta: ${counter}`}/>
+      <h1>{counter}</h1>
+      <h1>Dobro: {counter * 2}</h1>
+      <h1>Contador: {counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
