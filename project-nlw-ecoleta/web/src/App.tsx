954611a3b1768af7b068fd63e8793298510186b6
khayan <khayanmalantrucco@gmail.com>
2020-06-04 20:54:20 -0300

    Cleaning App Component and removing Header Component

--- project-nlw-ecoleta/web/src/App.tsx
+++ project-nlw-ecoleta/web/src/App.tsx
@@ -1,23 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-  
   return (
-    <div>
-      <Header title={`Ecoleta: ${counter}`}/>
-      <h1>{counter}</h1>
-      <h1>Dobro: {counter * 2}</h1>
-      <h1>Contador: {counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <h1>hello</h1>
   );
 }
 
