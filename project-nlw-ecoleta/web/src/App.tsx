edfe571c14e52bb910d39a98f5d7e38377f37fa5
khayan <khayanmalantrucco@gmail.com>
2020-06-05 02:33:18 -0300

    Added CreatePoint component

--- project-nlw-ecoleta/web/src/App.tsx
+++ project-nlw-ecoleta/web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home'
+// import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
 
 function App() {
   return (
-    <Home />
+    // <Home />
+    <CreatePoint />
   );
 }
 
---
+++ project-nlw-ecoleta/web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css'; 
+
+const CreatePoint = () => {
+    return <h1>Cadastre um Ponto de Coleta</h1>;
+}
+
+export default CreatePoint;
