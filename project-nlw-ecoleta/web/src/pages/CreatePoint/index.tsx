8997a8f06d66f9305d55208963e5c15621da07b0
khayan <khayanmalantrucco@gmail.com>
2020-06-06 15:29:51 -0300

    Added map in create point form page

--- project-nlw-ecoleta/web/src/pages/CreatePoint/index.tsx
+++ project-nlw-ecoleta/web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg'
@@ -61,6 +62,14 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-23.947202,-46.3336866]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-23.947202,-46.3336866]} />
+                    </Map>
 
                     <div className="field-group">
                         
