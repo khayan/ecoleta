cf5fa768aa3a9d3e4a2e08db80f1a9dfb7b45860
khayan <khayanmalantrucco@gmail.com>
2020-06-06 19:08:59 -0300

    Setting initial position from geolocation

--- project-nlw-ecoleta/web/src/pages/CreatePoint/index.tsx
+++ project-nlw-ecoleta/web/src/pages/CreatePoint/index.tsx
@@ -30,7 +30,15 @@ const CreatePoint = () => {
 
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setInitialPosition([latitude, longitude]);
+        });
+    }, []);
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -129,7 +137,7 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map center={[-23.947202,-46.3336866]} zoom={15} onclick={handleMapClick}>
+                    <Map center={initialPosition} zoom={15} onclick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
