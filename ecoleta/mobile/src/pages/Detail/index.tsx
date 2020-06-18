365b324128c558f10b1d47d823920544ac6957c7
khayan <khayanmalantrucco@gmail.com>
2020-06-17 21:59:30 -0300

    Added expo-mail-composer, WhatsApp and E-mail buttons on Details page done

--- ecoleta/mobile/src/pages/Detail/index.tsx
+++ ecoleta/mobile/src/pages/Detail/index.tsx
@@ -3,7 +3,8 @@ import Constants from 'expo-constants';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import api from '../../services/api';
 
@@ -43,6 +44,17 @@ const Detail = () => {
         navigation.goBack();
     }
 
+    function handleComposeMail() {
+      MailComposer.composeAsync({
+        subject: 'Ecoleta | Como descartar?',
+        recipients: [data.point.email]
+      });
+    }
+
+    function handleWhatsApp() {
+      Linking.openURL(`whatsapp://send?phone${data.point.whatsapp}&text=Estou utilizando o Ecoleta App e gostaria de saber como posso descartar corretamente meus resíduos?`);
+    }
+
     if(!data.point) {
       return null; // loading screen
     }
@@ -67,12 +79,12 @@ const Detail = () => {
             </View>
 
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleWhatsApp}>
                     <FontAwesome name="whatsapp" size={20} color="#FFF" />
                     <Text style={styles.buttonText}>WhatsApp</Text>
                 </RectButton>
 
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleComposeMail}>
                     <Icon name="mail" size={20} color="#FFF" />
                     <Text style={styles.buttonText}>E-mail</Text>
                 </RectButton>
